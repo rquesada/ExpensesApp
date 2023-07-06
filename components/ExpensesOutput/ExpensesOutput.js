86b958cfdfadb8061164852febb437bba5a736d7
Roy Quesada <roy.quesada@gmail.com>
2023-07-05 19:44:00 -0600

    add styles

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,20 +1,28 @@
 import { 
     View, 
-    StyleSheet
+    StyleSheet,
+    Text
 } from "react-native";
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
 import { GlobalStyles } from './../../constants/styles';
 
 
-function ExpensesOutput({expenses, expensesPeriod}){
+function ExpensesOutput({expenses, expensesPeriod, fallbackText}){
+
+    let content = <Text style={styles.infoText}>{fallbackText}</Text>;
+
+    if(expenses.length > 0){
+        content = <ExpensesList expenses={expenses} />;
+    }
+
     return (
         <View style={styles.container}>
             <ExpensesSummary 
                 expenses={expenses} 
                 periodName={expensesPeriod} 
             />
-            <ExpensesList expenses={expenses} />
+            {content}
         </View>
     );
 }
@@ -28,5 +36,11 @@ const styles = StyleSheet.create({
         paddingTop: 24,
         paddingBottom: 0,
         backgroundColor: GlobalStyles.colors.primary700
+    },
+    infoText:{
+        color: 'white',
+        fontSize: 16,
+        textAlign: 'center',
+        marginTop: 32
     }
 });
